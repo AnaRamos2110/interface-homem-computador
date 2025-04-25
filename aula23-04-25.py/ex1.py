bda0fa3355646cb026c46331f423df63d85414da
ANA BEATRIZ RAMOS <ana.bramos4@senacsp.edu.br>
2025-04-25 19:51:21 -0300

    Revisão básica de python

---
+++ aula23-04-25.py/ex1.py
@@ -0,0 +1,11 @@
+'''Doc strings- Onde você escreve mais de uma linha de código!'''
+
+
+#A Hashtag para uma linha de código
+
+'''Função print: Tem como a finalidade de imprimir o resultado de algum valor de alguma variável ou string "pura", sem a necessidade de uma variável, vejamos o exemplo'''
+Model = 'T800'
+print("My model is:", Model)
+
+
+
---
+++ aula23-04-25.py/ex2.py
@@ -0,0 +1,2 @@
+'''Toda linguagem tem uma tipagem de dados, ou seja,a a forma de interpretar um dado e isso tem que ser especificado para a linguagem'''
+Hierarquia = "Float, Integer, String & Boolean os principais tipos!"
